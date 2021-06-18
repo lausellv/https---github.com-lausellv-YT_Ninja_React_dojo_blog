03dc5fd29f25ec1d5fb2bae5549333713627135e
Victor <lausellv@yahoo.com>
2021-06-18 19:34:15 +0200

    practicing adding a link using dynamic value

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
+  const link = "http://www.google.com";
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
+        <br></br>
+        <a href={link}> Google Site </a>
       </div>
+      
     </div>
   );
 }
