9ae77d67b22e7730904910f4afe62baecf3cda33
Victor <lausellv@yahoo.com>
2021-06-18 19:34:52 +0200

    using useState from 'react'

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useState } from "react";  // it's a hook to create a reactive value 
+
 const Home = () => {
-  // let name = "Mario";
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(27);
-  const handleClick = function () {
-    setName("luigi");
+
+ 
+  // let name = "Mario";  this is a non reactive variable 
+  const [name, setName] = useState("Mario");  // [initial value, function to change that value set<valueName>]
+  const [age, setAge] = useState(28);
+
+  const handleClickAgain = (name, e)=>{
+console.log('Hello '+ name, e.target)
+  }
+
+  const handleClick = function (e) {
+    console.log('hello ninjas', e.target)
+    setName("Luigi");
     setAge(45);
   };
 
@@ -12,9 +21,10 @@ const Home = () => {
     <div className="home">
       <h2>Homepage</h2>
       <p>
-        {name} is {age} years old
+        {name} is {age} years old.
       </p>
       <button onClick={handleClick}>Click me</button>
+      <button onClick={(e)=>handleClickAgain('Victor', e)}>Click me again</button>
     </div>
   );
 };
