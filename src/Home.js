689bcc7294f39d59510065e273172e3173c724a5
Victor <lausellv@yahoo.com>
2021-06-26 23:20:05 +0200

    added useState

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,16 @@
-import { useState } from "react";  // it's a hook to create a reactive value 
+import { useState } from "react";
+import BlogList from "./BlogList"; // it's a hook to create a reactive value
 
 const Home = () => {
-
- 
-  // let name = "Mario";  this is a non reactive variable 
-  const [name, setName] = useState("Mario");  // [initial value, function to change that value set<valueName>]
-  const [age, setAge] = useState(28);
-
-  const handleClickAgain = (name, e)=>{
-console.log('Hello '+ name, e.bubbles) 
-  }
-
-  const handleClick = function (e) {
-    console.log('hello ninjas', e)
-    setName("Luigi");
-    setAge(45);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 }
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e)=>handleClickAgain('Victor', e)}>Click me again</button>
-      <button onClick={(e)=>{console.log('hello', e.target)}}>Click me one more time</button>
+      <BlogList blogs={blogs} title="All Blogs!"/>
     </div>
   );
 };
